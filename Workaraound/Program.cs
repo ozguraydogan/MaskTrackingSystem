95000ba5dba3b6b2cb2a3a4291d373e27d3698d4
Özgür Aydoğan <61085808+ozguraydogan@users.noreply.github.com>
2024-02-04 11:30:15 -0800

    Training camp first project

---
+++ Workaraound/Program.cs
@@ -0,0 +1,6 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+
+Person p = new Person() { FirstName = "Ozgur", LastName = "Aydogan", DateOfBirthYear = 1400, Identity = 2 };
+PttManager pttManager = new PttManager(new PersonManager());
+pttManager.GiveMask(p);
